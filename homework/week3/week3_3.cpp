5fe364e6bb2aa72162debf85a44ef75500dacdd0
yand-dongmin <dream1578@naver.com>
2026-03-26 01:32:08 +0900

    과제 완

--- homework/week3/week3_3.cpp
+++ homework/week3/week3_3.cpp
@@ -3,26 +3,34 @@
 using namespace std;
 
 int main() {
-    string s, sub;
-    int len, count, startIndex = 0;
+    string str, sub;
+    int len, plus;
+    int start = 0, sum = 0;
+
 
     cout << "덧셈식 입력 : ";
-    getline(cin, s);
-    len = s.length();
+    getline(cin, str);
+    len = str.length();
 
-    startIndex = s.find('+'); //1+22+3
-
-    for(int i = 0; i < len; i++){
-        startIndex = s.find('+', i);
-        if (startIndex == -1) // + 아닐 때
+    while(true){
+        plus = str.find('+', start); // + 찾기
+        if (plus == -1) // + 없을 때
         {
-            count++;
+            if (start < len) // 마지막 + 방지
+            {
+                sub = str.substr(start, plus - start);
+                sum += stoi(sub);
+                cout << sub << endl;
+            }
+            cout << "숫자들의 합은 " << sum << endl;
+            break;
         }
         else{ // + 일 때
-            sub = s.substr(startIndex,count);
+            sub = str.substr(start, plus - start);
+            sum += stoi(sub);
             cout << sub << endl;
         }
-        
+        start = plus + 1; // + 다음 인덱스
     }
 
 
